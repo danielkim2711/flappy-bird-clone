a8f3b28327a9850bf79298584f074c8faaa2d588
Daniel Kim <danielkim2711@gmail.com>
2021-12-12 23:07:02 +1300

    Set up pipes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { updateBird, setupBird, getBirdRect } from './bird.js';
-import { updatePipes } from './pipe.js';
+import { updatePipes, setupPipes } from './pipe.js';
 
 document.addEventListener('keypress', handleStart, { once: true });
 const title = document.querySelector('[data-title]');
@@ -30,6 +30,7 @@ function checkLose() {
 function handleStart() {
   title.classList.add('hide');
   setupBird();
+  setupPipes();
   lastTime = null;
   window.requestAnimationFrame(updateLoop);
 }
